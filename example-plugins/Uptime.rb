f4101b4b8dcac2c6fda17dd1aa8f166843d2a0be
Zeerix <zeerix@draig.de>
2011-07-07 01:03:25 +0200

    * Added Uptime, an example plugin which displays the time since server start

---
+++ example-plugins/Uptime.rb
@@ -0,0 +1,36 @@
+# Display time since server start or reload 
+
+Plugin.is {
+    name "Uptime"
+    version "0.1"
+    author "Zeerix"
+    commands :uptime => {
+        :usage => "/uptime - display time since server start or reload"
+    }
+}
+
+# use permission provider
+require 'bukkit/permissions'
+
+# plugin class
+class Uptime < RubyPlugin
+    def onEnable
+        @serverStart = time
+        print "[" + description.name + "] " + description.fullName + " enabled."
+    end
+    
+    def onDisable; print uptimeString; end
+    
+    def onCommand(sender, command, label, args)
+        if !sender.isPlayer || sender.has("rubybukkit.uptime") then
+            sender.sendMessage uptimeString
+        end
+        true
+    end
+
+    def time; java.lang.System.currentTimeMillis / 1000; end     
+    def uptimeString
+		diff = time - @serverStart
+		"Uptime: #{diff / 86400}d #{diff / 3600 % 24}h #{diff / 60 % 60}m #{diff % 60}s"
+    end     
+end
